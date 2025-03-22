f26875e38e4f54457c58d138839e634ac3902905
19Tyom98 <149366851+19Tyom98@users.noreply.github.com>
2025-03-22 10:23:24 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Header from './Header'
 
 export default function Contact() {
   return (
-    <div className='contact'>
+    <div className='contact' style{{background : 'red'}}>
       <h1>kjedgsch</h1>
       
     </div>
