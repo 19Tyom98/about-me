7609d4180fc5965a6727bc5e2f292e8c2a3dd078
19Tyom98 <149366851+19Tyom98@users.noreply.github.com>
2025-07-30 11:50:16 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import '../components/Contact.css'
-// import { pick2 } from './alllist'
-// import { contact_col } from './alllist'
+import { BsTelephone } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoLocationOutline } from "react-icons/io5";
 
 export default function Contact() {
+
+const pick2 = [{pickchar2 : './images/meee.jpg'}]
+
+ const contact_col = [
+    {
+        logo : <BsTelephone />,
+        name : 'Phone Number',
+        ipee : '+374 41130297'
+    },
+    {
+        logo : <AiOutlineMail />,
+        name : 'Email Address',
+        ipee : 'ghukasyantyom79@gmail.com'
+    },
+    {
+    logo : <FaWhatsapp />,
+    name : 'Whatsapp',
+    ipee : '+374 41130297'
+    },
+    {
+    logo : <IoLocationOutline />,
+    name : 'Our Office',
+    ipee : 'Respublic Of Armenia'
+    },
+]
+
   return (
     <div className='contact_block'>
       <div className="name_page">
@@ -12,7 +40,7 @@ export default function Contact() {
 
       <div className="constructor_block">
         <div className="images">
-             {/* {
+             {
                  contact_col.map(col => {
                   return <div className="info_block">
                       {col.logo}
@@ -20,15 +48,15 @@ export default function Contact() {
                       <p>{col.ipee}</p>
                   </div>
                  })
-              } */}
+              }
        </div>
-       {/* {
+       {
         pick2.map(ite => {
           return <div className="pickchar">
             <img src={ite.pickchar2} alt="" />
           </div>
         })
-       } */}
+       }
 
        <div className="text_contact">
         <h2>Hello World</h2>
@@ -41,5 +69,4 @@ Getting in touch is easy: simply use the contact details below.</p>
     </div>
   )
 }
-
 
