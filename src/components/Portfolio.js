22405d4c4b79fd36997b8e17b58b809425825b16
19Tyom98 <149366851+19Tyom98@users.noreply.github.com>
2025-07-31 08:39:29 -0700

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from './Header'
+import { FaFaceFrownOpen } from "react-icons/fa6";
 
 export default function Portfolio() {
   return (
-    <div className='portfolio'>
-      {/* <Header /> */}
+    <div className='Portfolio'>
+        <h1>"The page is temporarily unavailable due to technical issues."</h1>
+        <FaFaceFrownOpen style={{fontSize : '5em'}}/>
+       <h2>SORRY...</h2>
     </div>
   )
 }
